f280d2d10816b4366f7c1ce6cf0e4682c99b85fb
Flavio Lima <flaviolima.s@live.com>
2021-06-17 19:54:41 -0300

    Classe temporaria para testar

---
+++ src/br/com/curso/campoMinado/visao/Temp.java
@@ -0,0 +1,10 @@
+package br.com.curso.campoMinado.visao;
+
+import br.com.curso.campoMinado.modelo.Tabuleiro;
+
+public class Temp {
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro(3, 3, 9);
+        tabuleiro.abrir(2, 2);
+    }
+}
