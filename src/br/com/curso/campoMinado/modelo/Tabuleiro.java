88438c3d2af324a68db9652dc7d3cf9fd1301602
Flavio Lima <flaviolima.s@live.com>
2021-06-17 19:59:30 -0300

    Validacao se houve explosao

--- src/br/com/curso/campoMinado/modelo/Tabuleiro.java
+++ src/br/com/curso/campoMinado/modelo/Tabuleiro.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
     private int linhas;
     private int colunas;
     private int minas;
@@ -75,5 +75,13 @@ public class Tabuleiro {
     public void reiniciar() {
         campos.stream().forEach(c -> c.reiniciar());
         sortearMinas();
+    }
+    
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        if (evento == CampoEvento.EXPLODIR) {
+            System.out.println("Perdeu... :(");
+        } else if (objetivoAlcancado()) {
+            System.out.println("Ganhou... :)");
+        }
     }
 }
