cb3b4aa0d2bd6e245a0520f125a8adfc510461e1
Flavio Lima <flaviolima.s@live.com>
2021-06-18 15:31:07 -0300

    definicao de construtor a atributo

--- src/br/com/curso/campoMinado/modelo/ResultadoEvento.java
+++ src/br/com/curso/campoMinado/modelo/ResultadoEvento.java
@@ -2,4 +2,11 @@ package br.com.curso.campoMinado.modelo;
 
 public class ResultadoEvento {
     
+    private final boolean ganhou;
+
+    public ResultadoEvento(boolean ganhou) {
+        this.ganhou = ganhou;
+    }
+
+    
 }
