af5fcf6c9b1d3cbcac72651ff748b048a77a1a4b
Flavio Lima <flaviolima.s@live.com>
2021-06-18 18:09:05 -0300

    chamada para registrar observador

--- src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
+++ src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
@@ -11,7 +11,9 @@ public class PainelTabuleiro extends JPanel {
     public PainelTabuleiro(Tabuleiro tabuleiro) {
 
         setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
-
         tabuleiro.iterarCampos(c -> add(new BotaoCampo(c)));
+        tabuleiro.registrarObservador(e -> {
+            // TODO mostrar resultado para usuário
+        });
     }
 }
