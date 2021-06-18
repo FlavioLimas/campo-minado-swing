f4a8a8dbd3b452f4c0d05d5692965d0e66d77c15
Flavio Lima <flaviolima.s@live.com>
2021-06-18 15:56:36 -0300

    Criando tabuleiro e seus campos

--- src/br/com/curso/campoMinado/visao/TelaPrincipal.java
+++ src/br/com/curso/campoMinado/visao/TelaPrincipal.java
@@ -2,9 +2,14 @@ package br.com.curso.campoMinado.visao;
 
 import javax.swing.JFrame;
 
+import br.com.curso.campoMinado.modelo.Tabuleiro;
+
 public class TelaPrincipal extends JFrame {
     
     public TelaPrincipal() {
+
+        Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+        
         setTitle("Campo Minado");
         setSize(690, 438);
         setLocationRelativeTo(null);
