3374a490d27a6d6116c1f106ca305e2981fcd745
Flavio Lima <flaviolima.s@live.com>
2021-06-18 16:01:46 -0300

    definindo construtor ao painelTabuleiro

--- src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
+++ src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
@@ -2,6 +2,11 @@ package br.com.curso.campoMinado.visao;
 
 import javax.swing.JPanel;
 
+import br.com.curso.campoMinado.modelo.Tabuleiro;
+
 public class PainelTabuleiro extends JPanel {
     
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+        
+    }
 }
