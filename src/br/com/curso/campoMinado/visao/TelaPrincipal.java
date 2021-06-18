9bdcc84d6f2a059236a999a0ef4cfbc794f56c50
Flavio Lima <flaviolima.s@live.com>
2021-06-18 15:51:15 -0300

    Setando parametros ao instanciar tela

--- src/br/com/curso/campoMinado/visao/TelaPrincipal.java
+++ src/br/com/curso/campoMinado/visao/TelaPrincipal.java
@@ -4,6 +4,12 @@ import javax.swing.JFrame;
 
 public class TelaPrincipal extends JFrame {
     
+    public TelaPrincipal() {
+        setTitle("Campo Minado");
+        setSize(690, 438);
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
         new TelaPrincipal();
     }
