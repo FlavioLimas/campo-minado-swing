2fe9a69d8d30e7251bd7cf54d389d847e55c8b76
Flavio Lima <flaviolima.s@live.com>
2021-06-18 17:51:34 -0300

    Definindo componente

---
+++ src/br/com/curso/campoMinado/visao/BotaoCampo.java
@@ -0,0 +1,13 @@
+package br.com.curso.campoMinado.visao;
+
+import javax.swing.JButton;
+
+import br.com.curso.campoMinado.modelo.Campo;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+        
+    }
+    
+}
