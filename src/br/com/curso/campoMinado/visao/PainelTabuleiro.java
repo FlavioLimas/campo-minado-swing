59872f10fcbac213bfa258e80f5f8ffe346fee23
Flavio Lima <flaviolima.s@live.com>
2021-06-18 17:49:13 -0300

    definindo botoes da grid

--- src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
+++ src/br/com/curso/campoMinado/visao/PainelTabuleiro.java
@@ -2,6 +2,7 @@ package br.com.curso.campoMinado.visao;
 
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import br.com.curso.campoMinado.modelo.Tabuleiro;
@@ -11,5 +12,10 @@ public class PainelTabuleiro extends JPanel {
     public PainelTabuleiro(Tabuleiro tabuleiro) {
 
         setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        int total = tabuleiro.getLinhas() * tabuleiro.getColunas();
+        for (int i = 0; i < total; i++) {
+            add(new JButton());
+        }
     }
 }
