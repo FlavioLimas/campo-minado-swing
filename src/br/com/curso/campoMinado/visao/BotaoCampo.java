f87bd933b8d62f7a68c6ba15b5d5d747b9e00b1a
Flavio Lima <flaviolima.s@live.com>
2021-06-21 16:47:15 -0300

    implementando interface e sobrescrevendo metodo

--- src/br/com/curso/campoMinado/visao/BotaoCampo.java
+++ src/br/com/curso/campoMinado/visao/BotaoCampo.java
@@ -3,11 +3,27 @@ package br.com.curso.campoMinado.visao;
 import javax.swing.JButton;
 
 import br.com.curso.campoMinado.modelo.Campo;
+import br.com.curso.campoMinado.modelo.CampoEvento;
+import br.com.curso.campoMinado.modelo.CampoObservador;
 
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
 
     public BotaoCampo(Campo campo) {
         
     }
     
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        switch (evento) {
+            case ABRIR:
+                aplicarEstiloAbrir();
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    private void aplicarEstiloAbrir() {
+    }
 }
